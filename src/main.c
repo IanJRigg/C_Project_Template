8dcf2280ed2535b0127a67214fcce3e7a44e859e
Ian <ianjrigg@gmail.com>
2019-07-16 14:05:15 -0700

    Add signal handling mechanism to the template

--- src/main.c
+++ src/main.c
@@ -1,8 +1,201 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+
+volatile sig_atomic_t Received_SIGABRT = 0L;
+volatile sig_atomic_t Received_SIGFPE  = 0L;
+volatile sig_atomic_t Received_SIGHUP  = 0L;
+volatile sig_atomic_t Received_SIGILL  = 0L;
+volatile sig_atomic_t Received_SIGINT  = 0L;
+volatile sig_atomic_t Received_SIGKILL = 0L;
+volatile sig_atomic_t Received_SIGPIPE = 0L;
+volatile sig_atomic_t Received_SIGQUIT = 0L;
+volatile sig_atomic_t Received_SIGTERM = 0L;
+volatile sig_atomic_t Received_SIGUSR1 = 0L;
+volatile sig_atomic_t Received_SIGUSR2 = 0L;
+
+/**********************************************************************************************//**
+ * \brief Signal handler for every catchable signal
+ *  \param signal Incoming POSIX signal identifier
+ *************************************************************************************************/
+static void handle_signal(int signal)
+{
+    switch(signal)
+    {
+        case SIGABRT: Received_SIGABRT = signal; break;
+        case SIGFPE:  Received_SIGFPE  = signal; break;
+        case SIGHUP:  Received_SIGHUP  = signal; break;
+        case SIGILL:  Received_SIGILL  = signal; break;
+        case SIGINT:  Received_SIGINT  = signal; break;
+        case SIGPIPE: Received_SIGPIPE = signal; break;
+        case SIGQUIT: Received_SIGQUIT = signal; break;
+        case SIGTERM: Received_SIGTERM = signal; break;
+        case SIGUSR1: Received_SIGUSR1 = signal; break;
+        case SIGUSR2: Received_SIGUSR2 = signal; break;
+
+        case SIGSEGV:
+        default:
+            _exit(1L); // Very important, exit(1L) is not re-entrant, but _exit(1L) is
+            break;
+    }
+}
+
+/**********************************************************************************************//**
+ * \brief Utility function to check if a recoverable signal has been caught
+ *  \return true if no signals have been caught
+ *  \return false if a signal has been caught
+ *************************************************************************************************/
+static bool no_signals_have_been_raised()
+{
+    if(Received_SIGABRT != 0L)
+    {
+        printf("Received SIGABRT\n");
+        return false;
+    }
+
+    if(Received_SIGFPE  != 0L)
+    {
+        printf("Received SIGFPE\n");
+        return false;
+    }
+
+    if(Received_SIGHUP  != 0L)
+    {
+        printf("Received SIGHUP\n");
+        return false;
+    }
+
+    if(Received_SIGILL  != 0L)
+    {
+        printf("Received SIGILL\n");
+        return false;
+    }
+
+    if(Received_SIGINT  != 0L)
+    {
+        printf("Received SIGINT\n");
+        return false;
+    }
+
+    if(Received_SIGPIPE != 0L)
+    {
+        printf("Received SIGPIPE\n");
+        return false;
+    }
+
+    if(Received_SIGQUIT != 0L)
+    {
+        printf("Received SIGQUIT\n");
+        return false;
+    }
+
+    if(Received_SIGTERM != 0L)
+    {
+        printf("Received SIGTERM\n");
+        return false;
+    }
+
+    if(Received_SIGUSR1 != 0L)
+    {
+        printf("Received SIGUSR1\n");
+        return false;
+    }
+
+    if(Received_SIGUSR2 != 0L)
+    {
+        printf("Received SIGUSR2\n");
+        return false;
+    }
+
+    return true;
+}
+
+/**********************************************************************************************//**
+ * \brief Signal handler initialization function
+ *************************************************************************************************/
+static void setup_signal_handlers()
+{
+    struct sigaction sa;
+    memset(&sa, 0U, sizeof(sa));
+
+    sa.sa_handler = handle_signal;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
+
+    if(sigaction(SIGABRT, &sa, NULL) == -1L)
+    {
+        perror("SIGABRT");
+        exit(1L);
+    }
+
+    if(sigaction(SIGFPE,  &sa, NULL) == -1L)
+    {
+        perror("SIGFPE");
+        exit(1L);
+    }
+
+    if(sigaction(SIGHUP,  &sa, NULL) == -1L)
+    {
+        perror("SIGHUP");
+        exit(1L);
+    }
+
+    if(sigaction(SIGILL,  &sa, NULL) == -1L)
+    {
+        perror("SIGILL");
+        exit(1L);
+    }
+
+    if(sigaction(SIGINT,  &sa, NULL) == -1L)
+    {
+        perror("SIGINT");
+        exit(1L);
+    }
+
+    if(sigaction(SIGPIPE, &sa, NULL) == -1L)
+    {
+        perror("SIGPIPE");
+        exit(1L);
+    }
+
+    if(sigaction(SIGQUIT, &sa, NULL) == -1L)
+    {
+        perror("SIGQUIT");
+        exit(1L);
+    }
+
+    if(sigaction(SIGTERM, &sa, NULL) == -1L)
+    {
+        perror("SIGTERM");
+        exit(1L);
+    }
+
+    if(sigaction(SIGUSR1, &sa, NULL) == -1L)
+    {
+        perror("SIGUSR1");
+        exit(1L);
+    }
+
+    if(sigaction(SIGUSR2, &sa, NULL) == -1L)
+    {
+        perror("SIGUSR2");
+        exit(1L);
+    }
+
+    if(sigaction(SIGSEGV, &sa, NULL) == -1L)
+    {
+        perror("SIGSEGV");
+        exit(1L);
+    }
+}
 
 int main(int argc, char** argv)
 {
+    setup_signal_handlers();
+
     printf("I'm not doing anything right now!\n");
     return EXIT_SUCCESS;
 }
