c74699b14ba517f680fc0f560de4442e4860a1ad
Ian <ianjrigg@gmail.com>
2019-07-16 12:58:44 -0700

    Update project template with CMake3, Catch2, Doxygen, and a set of utility scripts

--- src/main.c
+++ src/main.c
@@ -1,26 +1,8 @@
 #include <stdio.h>
-#include <getopt.h>
 #include <stdlib.h>
 
 int main(int argc, char** argv)
 {
-    printf("Currently doing nothing\n");
-    if(argc == 2L)
-    {
-        int option = 0L;
-        while((option = getopt(argc, argv, "")) != -1L)
-        {
-            switch(option)
-            {
-                default:
-                    break;
-            }
-        }
-    }
-    else
-    {
-        exit(EXIT_FAILURE);
-    }
-
+    printf("I'm not doing anything right now!\n");
     return EXIT_SUCCESS;
 }
---
+++ test/test-runner.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+extern "C"
+{
+    // Add C-Style Headers Here!
+}
+
+TEST_CASE("Basic Checkout")
+{
+    REQUIRE(true);
+}
